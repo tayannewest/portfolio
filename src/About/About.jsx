85d30c3bb34d3a3fea6606c66be7a5daa7d740c1
Tay West <tayanne.west@gmail.com>
2021-12-30 23:32:12 -0500

    edit a lil of the work copy

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -5,6 +5,7 @@ const About = () => {
   return (
     <div className="about">
       This part is the small intro to meeeeee
+      Skills and background and interests and stuff
     </div>
   )
 }
--- src/Works/Works.jsx
+++ src/Works/Works.jsx
@@ -6,19 +6,26 @@ const Works = () => {
       <div className="all-work">
         <div className="work odd">
           <a href="http://swatchdog.herokuapp.com/">SwatchDog</a>
-          A work example
+          An online swatch library for artists <br />
+          Django | AWS S3 | PostgreSQL
         </div>
         <div className="work even">
           <a href="http://lfgpls.herokuapp.com/">Looking For Group</a>
-          A work example
+          A social app for the solo adventurer
+          <br />
+          ReactJs | Axios | Yelp Fusion
         </div>
         <div className="work odd">
           <a href="http://renannymation.netlify.app">ReNannyMation</a>
-          A work example
+          An unconventional babysitting simulator
+          <br />
+          Javascript | CSS | HTML
         </div>
         <div className="work even">
           <a href="https://good-car-ideas.herokuapp.com/">Focus Group</a>
-          A work example
+          An ode to Tim Robinson
+          <br />
+          EJS | Mongoose | MongoDB
         </div>
         <div className="work odd">
           A work example

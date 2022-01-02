5de28b193e22ec8069c9e3cf1ca21daa2214a3f6
Tay West <tayanne.west@gmail.com>
2022-01-02 14:30:08 -0500

    hide menu after click

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -4,33 +4,62 @@ import "./Nav.css"
 // import { Link } from "react-scroll"
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
+class Nav extends React.Component {
 
-const Nav = () => {
+  constructor (props) {
+    super(props)
+    this.state = {
+      menuOpen: false
+    }
+  }
 
-  return (
-    <Menu right>
-      <AnchorLink 
-      href="#about" 
-      className="menu-item">
-        About Me
-      </AnchorLink>
-      <AnchorLink 
-      href="#works" 
-      className="menu-item">
-        Recent Projects
-      </AnchorLink>
-      <AnchorLink 
-      href="#skills" 
-      className="menu-item">
-        Skills and Technologies
-      </AnchorLink>
-      <AnchorLink 
-      href="#contact" 
-      className="menu-item">
-        Contact
-      </AnchorLink>
-    </Menu>
-  )
+  handleStateChange (state) {
+    this.setState({menuOpen: state.isOpen})  
+  }
+
+  closeMenu () {
+    this.setState({menuOpen: false})
+  }
+
+  toggleMenu () {
+    this.setState(state => ({menuOpen: !state.menuOpen}))
+  }
+
+  render () {
+
+    return (
+      <Menu 
+        right
+        isOpen={this.state.menuOpen}
+        onStateChange={(state) => this.handleStateChange(state)}>
+        <AnchorLink 
+        href="#about" 
+        className="menu-item"
+        onClick={() => this.closeMenu()}
+        >
+          About Me
+        </AnchorLink>
+        <AnchorLink 
+        href="#works" 
+        className="menu-item"
+        onClick={() => this.closeMenu()}>
+          Recent Projects
+        </AnchorLink>
+        <AnchorLink 
+        href="#skills" 
+        className="menu-item"
+        onClick={() => this.closeMenu()}>
+          Skills and Technologies
+        </AnchorLink>
+        <AnchorLink 
+        href="#contact" 
+        className="menu-item"
+        onClick={() => this.closeMenu()}>
+          Contact
+        </AnchorLink>
+      </Menu>
+    )
+  }
 }
 
 export default Nav

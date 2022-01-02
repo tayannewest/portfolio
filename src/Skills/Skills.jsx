0d607377edd113578f3f86b2ff9ef47187e4485a
Tay West <tayanne.west@gmail.com>
2022-01-02 12:41:46 -0500

    footer spacing and font sizing

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -3,12 +3,12 @@ import "./Skills.css"
 import js from "../Images/JavaScript-01.svg"
 import css from "../Images/CSS-3-01.svg"
 import html from "../Images/HTML-5-01.svg"
+import react from "../Images/reactjs-icon.svg"
 import node from "../Images/Node-JS-02.svg"
 import python from "../Images/Python-04.svg"
 import django from "../Images/Django-03.svg"
 import mongoDB from "../Images/mongodb-icon.svg"
 import jquery from "../Images/jquery_plain_logo_icon_146444.svg"
-import axios from "../Images/axios_logo_icon_168545.svg"
 import github from "../Images/github-tile.svg"
 
 
@@ -26,6 +26,10 @@ const Skills = () => {
       <div className="logo">
         <img src={html} alt="html logo" className="skill-logo"/>
         <h3>HTML 5</h3>
+      </div>
+      <div className="logo">
+        <img src={react} alt="react logo" className="skill-logo"/>
+        <h3>React</h3>
       </div>
       <div className="logo">
         <img src={node} alt="node logo" className="skill-logo"/>
@@ -46,10 +50,6 @@ const Skills = () => {
       <div className="logo">
         <img src={jquery} alt="jquery logo" className="skill-logo"/>
         <h3>jQuery</h3>
-      </div>
-      <div className="logo">
-        <img src={axios} alt="axios logo" className="skill-logo"/>
-        <h3>Axios</h3>
       </div>
       <div className="logo">
         <img src={github} alt="github logo" className="skill-logo"/>

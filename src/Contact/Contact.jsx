bd3e745c971171538eaf24583cc2b9e43cb8f9ce
Tay West <tayanne.west@gmail.com>
2022-01-03 16:16:40 -0500

    link to resume pdf

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
             Email me: 
             <a href="mailto:tayanne.west@gmail.com"> Email </a>
             <br />
-            See my experience: <a href="https://docs.google.com/document/d/1MeqHFGCsltH8sX5Giue024IBYjBj7RPrwDK55thUf1I/edit?usp=sharing">Resume</a>
+            See my experience: <a href="https://docs.google.com/viewer?url=https://docs.google.com/document/d/1MeqHFGCsltH8sX5Giue024IBYjBj7RPrwDK55thUf1I/export?format=pdf">Resume</a>
             <br />
             Connect with me:
             <a href="https://www.linkedin.com/in/tayannewest/"> LinkedIn </a>

1e98861bcfdd6d15874aa2532a517127b15d1c0e
Tay West <tayanne.west@gmail.com>
2022-01-27 20:32:14 -0500

    edit about

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -4,7 +4,7 @@ import "./About.css"
 const About = () => {
   return (
     <div className="about">
-      My name is Tay, and I'm a software developer currently based in Jacksonville, Florida. I'm passionate about the way design shapes people’s reactions to media and love learning new ways to simplify and streamline code. When I'm not programming.
+      My name is Tay, and I'm a software developer currently based in Jacksonville, Florida. I'm passionate about the way design shapes people’s reactions to media and love learning new ways to simplify and streamline code. When I'm not programming you can find me roller skating, gaming, or hanging out with my cats.
     </div>
   )
 }
